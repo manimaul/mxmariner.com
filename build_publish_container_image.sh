0b7300189c0ae476ec9308688b0454ad590f5fed
William Kamp <manimaul@gmail.com>
2023-05-16 10:59:39 -0700

    news

--- build_publish_container_image.sh
+++ build_publish_container_image.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-yarn global add create-react-app
-yarn global add react-scripts
-yarn add rx-lite
-yarn add react-router
-yarn add react-router-dom
+yarn install
 yarn build
 tag_postfix="$(date +"%m-%d-%y")_r0"
-docker build -t "manimaul/mxmariner.com:${tag_postfix}" .
-docker tag "manimaul/mxmariner.com:${tag_postfix}" "manimaul/mxmariner.com:latest"
-docker push "manimaul/mxmariner.com:latest"
-docker push "manimaul/mxmariner.com:${tag_postfix}"
+name="ghcr.io/manimaul/mxmariner.com"
+docker build -t "${name}:${tag_postfix}" .
+docker tag "${name}:${tag_postfix}" "${name}:latest"
+docker push "${name}:latest"
+docker push "${name}:${tag_postfix}"
